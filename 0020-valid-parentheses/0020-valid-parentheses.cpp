660326dcc7f7b8947c5c051eefc9cc0183b9fce9
Saras Gautam Bari <sarasbari07@gmail.com>
2026-08-07 19:03:19 +0530

    Time: 0 ms (100%), Space: 8.9 MB (38.15%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '(' || c == '[' || c == '{')
+                st.push(c);
+            else {
+                if (st.empty())
+                    return 0;
+                if (c == ')' && st.top() != '(')
+                    return 0;
+                if (c == ']' && st.top() != '[')
+                    return 0;
+                if (c == '}' && st.top() != '{')
+                    return 0;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
